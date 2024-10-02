7444cddf342a354416dc93b640289fa03b3b9eab
Daria <407638@niuitmo.ru>
2024-10-02 15:46:22 +0300

    comments've been added to the files rectangle.py, triangle.py, circle.py, square.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает длину радиуса, возращает площадь круга.'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает длину радиуса, возвращает длину окружности.'''
     return 2 * math.pi * r
 
